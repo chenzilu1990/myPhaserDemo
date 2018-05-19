f5c42a3611304b0d719b1237a371f36b80a9cb1c
chenzilu1990 <1991793085@qq.com>
2018-05-19 22:35:46 +0800

    爆炸动画和响应碰撞

--- demo1/js/game.js
+++ demo1/js/game.js
@@ -26,7 +26,8 @@ var config = {
 			bullets : null,
 			enemys : null,
 			lastFired : 0,
-			lastEnemy :  0
+			lastEnemy : 0,
+			frames : []
 		}
 	}
 
@@ -49,9 +50,22 @@ function preload() {
 	this.load.image('enemy', './images/enemy.png')
 	this.load.image('hero', './images/hero.png')
 	this.load.image('bullet', './images/bullet.png')
+	for(var i = 0; i < 19; i++){
+		// array[i]
+		// console.log('hrh' + (i + 1))
+		let key1 = 'explosion' + (i + 1)
+		let path = './images/explosion' + (i + 1) + '.png'
+		let frame = {
+			key : key1
+
+		}
+		this.frames.push(frame)
+		this.load.image(key1, path)
+	}
 }
 
 function create() {
+	
 	// 敌人
 	var Enemy = new Phaser.Class({
 		Extends : Phaser.Physics.Arcade.Image,
@@ -61,27 +75,25 @@ function create() {
 
 
 			this.setOrigin(0, 0)
-			// this.x = Phaser.Math.Between(0, screenW - this.width) 
-			// this.y = y
+			
 			this.speed = 0.4
 			this.born = 0
-			// scene.children.add(this)
+			// this.enableBody(true,0,0,true,true)
+			// this.setActive()
 		},
 
-		// fire : function () {
-			
-		// },
+		
 
 		update : function(time, delta) {
-			// console.log('*******')
+
 			this.y += this.speed * delta
 			this.born += delta
 			if (this.born > 2000 ) {
 				this.setActive(false)
 				this.setVisible(false)
-				// this.y = -this.height
+
 				this.born = 0 
-				// this.x = Phaser.Math.Between(0, screenW - this.width) 
+
 
 			}
 		}
@@ -139,10 +151,32 @@ function create() {
 	player.setBounce(0)
 		
 	// 子弹池实例
-	this.bullets = this.add.group({classType : Bullet, runChildUpdate : true})
+	this.bullets = this.physics.add.group({classType : Bullet, runChildUpdate : true})
+	// this.bullets.enableBody = true
+	// console.log(game)
 	// 敌人池水实例
-	this.enemys = this.add.group({classType : Enemy, runChildUpdate : true})
-	
+	this.enemys = this.physics.add.group({classType : Enemy, runChildUpdate : true})
+	// var enemy = this.physics.add.image(0,0,'enemy')
+	// console.log(enemy)
+	// console.log(new Enemy(this))
+	this.physics.add.overlap(player,this.enemys,function () {
+		console.log('****')
+	}, null, this)
+	// this.physics.add.collider(player, this.enemys)
+	this.physics.add.overlap(this.bullets ,this.enemys,function () {
+		console.log('^_^')
+	}, null, this)
+
+
+	console.log(this.frames)
+	this.anims.create({
+		key : 'explosion',
+		frames : this.frames,
+		frameRate : 30,
+		hideOnComplete : true
+		// repeat : -1
+	})
+	this.add.sprite(100,100,'explosion1').play('explosion')
 
 }
 
@@ -155,6 +189,9 @@ function update(time,delta) {
 		enemy.y = - enemy.height
 		enemy.setActive(true)
 		enemy.setVisible(true)
+		// enemy.enableBody(false,0,0,true,true)
+		// console.log(enemy)
+
 		this.lastEnemy = time + 600
 	}
 	// 背景逻辑更新
